7709c703ad58f98c132a6bd20af276539d429b98
Yaroslav Mudrevskyi <yarikyaroslav13200@gmail.com>
2024-07-22 17:24:32 +0300

    added async/await

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,15 +1,18 @@
-export function onSearchImages(inputValue) {
-  const BASE_URL = 'https://pixabay.com';
-  const END_POINT = '/api/';
-  const params = new URLSearchParams({
-    key: '45015838-73ef95672254612a035b0fb4a',
-    q: inputValue,
-    image_type: 'photo',
-    orientation: 'horizontal',
-    safesearch: true,
+import Axios from 'axios';
+
+const axios = Axios.create({
+  baseURL: 'https://pixabay.com',
+});
+
+export async function onSearchImages(inputValue) {
+  const res = await axios.get('/api/', {
+    params: {
+      key: '45015838-73ef95672254612a035b0fb4a',
+      q: inputValue,
+      image_type: 'photo',
+      orientation: 'horizontal',
+      safesearch: true,
+    },
   });
-  return fetch(`${BASE_URL}${END_POINT}?${params}`).then(value => {
-    if (!value.ok) throw new Error(value.status);
-    return value.json();
-  });
+  return res.data;
 }
--- src/main.js
+++ src/main.js
@@ -17,46 +17,44 @@ hideLoader();
 
 refs.formEl.addEventListener('submit', onFormSubmit);
 
-function onFormSubmit(e) {
-  e.preventDefault();
-  showLoader();
-  refs.markupEl.innerHTML = '';
-  const inputValue = refs.formEl.elements.image.value.trim();
-  if (!inputValue) return;
-  refs.formEl.reset();
-  onSearchImages(inputValue)
-    .then(value => {
-      if (!value.hits.length) {
-        iziToast.show({
-          title: 'Oops',
-          titleColor: '#FAFAFB',
-          message:
-            'Sorry, there are no images matching your search query. Please try again!',
-          messageColor: '#FAFAFB',
-          backgroundColor: '#EF4040',
-          position: 'topRight',
-          timeout: 3000,
-        });
-        return;
-      }
-      renderImages(value.hits);
-      gallery.refresh();
-    })
-    .catch(error => {
-      iziToast.error({
-        title: 'Error',
+async function onFormSubmit(e) {
+  try {
+    e.preventDefault();
+    showLoader();
+    refs.markupEl.innerHTML = '';
+    const inputValue = refs.formEl.elements.image.value.trim();
+    if (!inputValue) return;
+    refs.formEl.reset();
+    const res = await onSearchImages(inputValue);
+    if (!res.hits.length) {
+      iziToast.show({
+        title: 'Oops',
         titleColor: '#FAFAFB',
-        message: 'Sorry, something wrong',
-        position: 'topRight',
+        message:
+          'Sorry, there are no images matching your search query. Please try again!',
         messageColor: '#FAFAFB',
         backgroundColor: '#EF4040',
         position: 'topRight',
         timeout: 3000,
       });
-    })
-    .finally(() => {
-      hideLoader();
+      return;
+    }
+    renderImages(res.hits);
+    gallery.refresh();
+  } catch (error) {
+    iziToast.error({
+      title: 'Error',
+      titleColor: '#FAFAFB',
+      message: 'Sorry, something wrong',
+      position: 'topRight',
+      messageColor: '#FAFAFB',
+      backgroundColor: '#EF4040',
+      position: 'topRight',
+      timeout: 3000,
     });
+  } finally {
+    hideLoader();
+  }
 }
 
 function renderImages(arr) {
